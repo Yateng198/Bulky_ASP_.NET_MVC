7b219196a71948097baf51a44ae93e6e1e4e99dd
Yateng Geng <yatenggeng@gmail.com>
2023-05-11 21:45:47 -0400

    Razor Page Project Added

---
+++ BulkyWebRazor_temp/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Category name length must less than 30 charaters!")]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "DisplayOrder must be between 1-100!")]
+        public int DisplayOrder { get; set; }
+    }
+}
